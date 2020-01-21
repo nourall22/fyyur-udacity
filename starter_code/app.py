46a15b8b34433a949f39879693da487b7363b480
noureddineall22 <nidorigin@gmail.com>
2020-01-21 11:32:11 +0100

    added create artist

--- starter_code/app.py
+++ starter_code/app.py
@@ -64,6 +64,7 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String)
     shows = db.relationship('Show', backref='artist', lazy=True)
@@ -362,15 +363,32 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
-  return render_template('pages/home.html')
+  # create an artist
+  try:
+    form = ArtistForm()
+    artist = Artist(
+        name=form.name.data,
+        city=form.city.data,
+        state=form.state.data,
+        phone=form.phone.data,
+        genres=form.genres.data,
+        facebook_link=form.facebook_link.data,
+        website=form.website.data,
+        image_link=form.image_link.data,
+        seeking_venue=form.seeking_venue.data,
+        seeking_description=form.seeking_description.data,
+    )
+    db.session.add(artist)
+    db.session.commit()
+    # on successful db insert, flash success
+    flash('Artist ' + artist.name + ' was successfully listed!')
+    return render_template('pages/home.html')
+  except Exception as e:
+    flash(f"An error occurred. Artist {request.form['name']} could not be listed. Error: {e}")
+    db.session.rollback()
+    return render_template('pages/home.html')
+  finally:
+    db.session.close()
 
 
 #  Shows
